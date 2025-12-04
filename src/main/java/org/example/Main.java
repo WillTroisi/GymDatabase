1f3ce7a1c6418fa1ede30810e51fd3b8815f473f
WillTroisi <wmtroisi@loyola.edu>
2025-12-04 18:25:25 -0500

    Fixed MemberService

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,7 @@
 package org.example;
 import org.example.controller.MemberService;
-import org.example.model.dao.ClientDAO;
-import org.example.model.dao.MemberDAO;
-import org.example.model.entity.Client;
-import org.example.model.entity.Member;
-import org.example.view.MainFrame;
 
 import java.sql.*;
-import java.time.LocalDate;
-import java.util.SortedMap;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
