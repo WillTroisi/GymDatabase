3a743f74c5c81357251af7d26cb340935fe2e5c4
WillTroisi <wmtroisi@loyola.edu>
2025-12-04 18:22:15 -0500

    Fixed MemberService

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,32 +14,10 @@ import java.util.SortedMap;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws SQLException {
-//        MainFrame frame = new MainFrame();
-//        frame.setVisible(true);
 
-//        ClientDAO clientDAO = new ClientDAO();
-//        clientDAO.setTestDatabase();
-//
-//        Client client = new Client();
-//        client.setName("George");
-//        client.setEmail("wn@j.edu");
-//
-//
-//        clientDAO.create(client);
-
-//        MemberDAO memberDAO = new MemberDAO();
-//
-//        Member member = new Member();
-//        member.setName("Tom");
-//        member.setEmail("tro@tr.tro");
-//        member.setPhoneNumber("123-456-789");
-//        member.setDt_join(Date .valueOf(LocalDate.now()));
-//        memberDAO.create(member);
-//
-//        Member found = memberDAO.read(member.getID());
-//        System.out.println(found);
         MemberService memberService = new MemberService();
-        memberService.createMember("Hello", "123@3.4", "123-456-765", Date.valueOf(LocalDate.now()));
+        memberService.createMember("Hello", "123@3.4", "123-456-765");
+
     }
 
 }
--- src/main/java/org/example/controller/MemberService.java
+++ src/main/java/org/example/controller/MemberService.java
@@ -3,22 +3,22 @@ package org.example.controller;
 import org.example.model.dao.MemberDAO;
 import org.example.model.entity.Member;
 
+import java.sql.Date;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
+
 public class MemberService {
-    public Member createMember(String name, String email, String phoneNumber, Date dt_join) throws SQLException {
-        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
-        java.sql.Date date = new java.sql.Date(dt_join.getTime());
+    public Member createMember(String name, String email, String phoneNumber) throws SQLException {
 
         Member member = new Member();
         member.setName(name);
         member.setEmail(email);
         member.setPhoneNumber(phoneNumber);
-        member.setDt_join(date);
+        member.setDt_join(Date.valueOf(LocalDate.now()));
 
         MemberDAO memberDAO = new MemberDAO();
         memberDAO.create(member);
